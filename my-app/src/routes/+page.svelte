a5588b0b05a05d35fbba9bd04a721b1a6c4d643b
Cod3Andy <derangedjk75@gmail.com>
2023-07-31 05:01:28 +0300

    E-store without API

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import ProductCard from "$lib/productCard.svelte";
+
+	const products: Product[] = [
+		{
+			id: "1",
+			name: "Coffee",
+			price: 5,
+		},
+		{
+			id: "2",
+			name: "Tea",
+			price: 5,
+		},
+		{
+			id: "3",
+			name: "Milk",
+			price: 5,
+		},
+	];
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="grid grid-col-3 gap-4">
+		<div class="col-span-3">
+			<h1 class="text-4xl">SveltKit 1.0 Store</h1>
+		</div>
+		{#each products as product}
+		<ProductCard product={product}/>
+		{/each}
+		<div class="col-span-3">
+			<button class="btn variant-filled-primary">Checkout with Stripe API</button>
+		</div>
+	</div>
+</div>
